e9fbc6ff796957a088fd9b169b09bb6027b5f774
codeunlock <124934448+codeunlock@users.noreply.github.com>
2024-04-21 21:53:19 +0300

    peer shared_functionality

--- src/peer.py
+++ src/peer.py
@@ -1,12 +1,13 @@
 import socket 
 import json
 from random import random
-
+import os 
 class Peer:
      
-     def __init__(self, tracker_host, tracker_port):
+     def __init__(self, tracker_host, tracker_port,shared_directory):
           self.tracker_host = tracker_host
           self.tracker_port = tracker_port
+          self.shared_directory = shared_directory
 
      def send_message(self, message):
           with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
@@ -36,24 +37,38 @@ class Peer:
           return self.send_message(message)
 
 
+     def register_shared_files(self):
+          shared_files = os.listdir(self.shared_directory)
+          for filename in shared_files:
+               file_path = os.path.join(self.shared_directory, filename)
+               if os.path.isfile(file_path):
+                    self.register_file_with_tracker(filename)
 
+     def register_file_with_tracker(self, filename):
+          message = {'action': 'register_file', 'filename': filename}
+          response = self.send_message(message)
+          print(response)  # Print response from Tracker
 
 
 if __name__ == "__main__":
-     peer = Peer("localhost", 5000)  # Host and port of the Tracker
-     
+     peer = Peer("localhost", 5000, "/Users/alex/aueb/com_net/p2p/shared_directory")  # Host and port of the Tracker
+     peer.register_shared_files()
+     list_response = peer.list_files()
+     print(list_response)
      # Test peer registration
-     #register_response = peer.register("user1", "password1")
-     #print(register_response)
+     register_response = peer.register("user1", "password1")
+     print(register_response)
 
      login_respone = peer.login("user1", "password1")
      print(login_respone)
 
+     list_response = peer.list()
+     print(list_response)
 
 """ 
 TODO:
      register a new user  ~~~> DONE 
-     login 
+     login  ~~~> DONE 
      list available files 
      get details of a file 
      logout 

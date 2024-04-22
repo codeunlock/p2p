1e5e4038a2636f726b98a2736e023d09ee5e5d59
codeunlock <124934448+codeunlock@users.noreply.github.com>
2024-04-22 19:13:55 +0300

    token_id fix

--- src/peer.py
+++ src/peer.py
@@ -1,16 +1,12 @@
 import socket 
 import json
-from random import random
-import os 
+from random import randint
 
 class Peer:
-
      
-     def __init__(self, tracker_host, tracker_port,shared_directory):
+     def __init__(self, tracker_host, tracker_port):
           self.tracker_host = tracker_host
           self.tracker_port = tracker_port
-          self.shared_directory = shared_directory
-
 
      def send_message(self, message):
           with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
@@ -24,7 +20,8 @@ class Peer:
           return self.send_message(message)
      
      def login(self,user_name,password):
-          message = {'action': 'login', 'user_name': user_name, 'password': password}
+          token_id = randint(100000, 999999)  # Generate random token ID
+          message = {'action': 'login', 'user_name': user_name, 'password': password, 'token_id': str(token_id)}
           return self.send_message(message)
      
      def logout(self, token_id):
@@ -39,37 +36,21 @@ class Peer:
           message = {'action': 'details', 'filenae': filename}
           return self.send_message(message)
 
-
-     def register_shared_files(self):
-          shared_files = os.listdir(self.shared_directory)
-          for filename in shared_files:
-               file_path = os.path.join(self.shared_directory, filename)
-               if os.path.isfile(file_path):
-                    self.register_file_with_tracker(filename)
-
-     def register_file_with_tracker(self, filename):
-          message = {'action': 'register_file', 'filename': filename}
-          response = self.send_message(message)
-          print(response)  # Print response from Tracker
-
-
 if __name__ == "__main__":
-     peer = Peer("localhost", 5000, "/Users/alex/aueb/com_net/p2p/shared_directory")  # Host and port of the Tracker
-     peer.register_shared_files()
-     list_response = peer.list()
-     print(list_response)
+     peer = Peer("localhost", 5000)  # Host and port of the Tracker
+     
      # Test peer registration
-     register_response = peer.register("user1", "password1")
+     register_response = peer.register("user1", "pass1")
      print(register_response)
+     
+     # Test peer login
+     login_response = peer.login("user1", "pass1")
+     print(login_response)
 
-     login_respone = peer.login("user1", "password1")
-     print(login_respone)
-
-     list_response = peer.list()
-     print(list_response)
-
-
-""" 
+     # Test peer logout
+     logout_response = peer.logout(login_response.get('token_id'))
+     print(logout_response)
+"""
 TODO:
      register a new user  ~~~> DONE 
      login  ~~~> DONE 

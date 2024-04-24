7a38d42aa69ba3266fdb2fffc27d2dd5ae98e28f
codeunlock <codeunlock1337@gmail.com>
2024-04-24 15:25:20 +0300

    shared_directory folder added

--- src/peer.py
+++ src/peer.py
@@ -1,118 +1,116 @@
-import socket 
+import os
+import socket
 import json
 import random
 
 class Peer:
-     def __init__(self, tracker_host, tracker_port):
-          self.tracker_host = tracker_host
-          self.tracker_port = tracker_port
-          self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          self.client_socket.settimeout(5)  # Set a timeout of 5 seconds
-          try:
-               self.client_socket.connect((self.tracker_host, self.tracker_port))
-          except Exception as e:
-               print("Error connecting to tracker:", e)
+    def __init__(self, tracker_host, tracker_port, shared_directory):
+        self.tracker_host = tracker_host
+        self.tracker_port = tracker_port
+        self.shared_directory = shared_directory
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.settimeout(5)  # Set a timeout of 5 seconds
+        try:
+            self.client_socket.connect((self.tracker_host, self.tracker_port))
+        except Exception as e:
+            print("Error connecting to tracker:", e)
 
-     def send_message(self, message):
-          try:
-               print("Sending message:", message)  # Add this line for debugging
-               self.client_socket.sendall(json.dumps(message).encode())
-               response = self.client_socket.recv(1024)
-               print("Received response:", response)  # Add this line for debugging
-               return json.loads(response.decode())
-          except socket.timeout:
-               print("Socket operation timed out.")
-               return {'status': 'error', 'message': 'Socket operation timed out.'}
-          except Exception as e:
-               print("Error:", e)
-               return {'status': 'error', 'message': str(e)}
+    def send_message(self, message):
+        try:
+            print("Sending message:", message)  # Add this line for debugging
+            self.client_socket.sendall(json.dumps(message).encode())
+            response = self.client_socket.recv(1024)
+            print("Received response:", response)  # Add this line for debugging
+            return json.loads(response.decode())
+        except socket.timeout:
+            print("Socket operation timed out.")
+            return {'status': 'error', 'message': 'Socket operation timed out.'}
+        except Exception as e:
+            print("Error:", e)
+            return {'status': 'error', 'message': str(e)}
 
-     def register(self, user_name, password):
-          message = {'action': 'register', 'user_name': user_name, 'password': password}
-          return self.send_message(message)
+    def register(self, user_name, password):
+        message = {'action': 'register', 'user_name': user_name, 'password': password}
+        register_response = self.send_message(message)
 
-     def login(self, user_name, password):
-          message = {'action': 'login', 'user_name': user_name, 'password': password}
-          login_response = self.send_message(message)
-          token_id = login_response.get('token_id', None)  # Extract token ID from login response
-          return token_id
+        if register_response['status'] == 'success':
+            # Create a new directory for the peer in the shared_directory
+            peer_directory = os.path.join(self.shared_directory, f"Peer{user_name}")
+            os.makedirs(peer_directory, exist_ok=True)
 
-     def logout(self, token_id):
-          message = {'action': 'logout', 'token_id': token_id}
-          print("Tracker host:", self.tracker_host)  # Add this line for debugging
-          print("Tracker port:", self.tracker_port)  # Add this line for debugging
-          print("Sending logout request:", message)  # Add this line for debugging
-          logout_response = self.send_message(message)
-          print("Received logout response:", logout_response)  # Add this line for debugging
-          return logout_response
+            # Add dummy files to the peer's directory
+            for i in range(2):
+                with open(os.path.join(peer_directory, f"file{i}.txt"), 'w') as f:
+                    f.write(f"Dummy file {i} for Peer{user_name}")
 
+        return register_response
 
-     def list(self):
-          message = {'action': 'list'}
-          return self.send_message(message)
+    def login(self, user_name, password):
+        message = {'action': 'login', 'user_name': user_name, 'password': password}
+        login_response = self.send_message(message)
+        token_id = login_response.get('token_id', None)  # Extract token ID from login response
+        return token_id
 
-     def details(self, filename):
-          message = {'action': 'details', 'filename': filename}
-          return self.send_message(message)
+    def logout(self, token_id):
+        message = {'action': 'logout', 'token_id': token_id}
+        logout_response = self.send_message(message)
+        return logout_response
 
-     def __del__(self):
-          self.client_socket.close()  # Close the socket when Peer instance is deleted
+    def list(self):
+        message = {'action': 'list'}
+        return self.send_message(message)
 
+    def details(self, filename):
+        message = {'action': 'details', 'filename': filename}
+        return self.send_message(message)
 
+    def __del__(self):
+        self.client_socket.close()  # Close the socket when Peer instance is deleted
 
 if __name__ == "__main__":
-     peer = Peer("localhost", 5000)  # Host and port of the Tracker
-    
-     while True:
-          print("1. Register")
-          print("2. Login")
-          print("3. Logout")
-          print("4. List available files")
-          print("5. Get details of a file")
-          print("6. Exit")
+    shared_directory = "/home/kafka/p2p/src/shared_directory"
+    peer = Peer("localhost", 5000, shared_directory)  # Host and port of the Tracker
 
-               
-          choice = input("Enter your choice: ")
+    while True:
+        print("1. Register")
+        print("2. Login")
+        print("3. Logout")
+        print("4. List available files")
+        print("5. Get details of a file")
+        print("6. Exit")
 
-          if choice == "1":
-               username = input("\nEnter username: ")
-               password = input("\nEnter password: ")
-               register_response = peer.register(username, password)
-               print(register_response,"\n")
+        choice = input("Enter your choice: ")
 
-          elif choice == "2":
-               username = input("\nEnter username: ")
-               password = input("\nEnter password: ")
-               login_response = peer.login(username, password)
-               print("\nYour connection's ID is:",login_response,"\n")
+        if choice == "1":
+            username = input("\nEnter username: ")
+            password = input("\nEnter password: ")
+            register_response = peer.register(username, password)
+            print(register_response,"\n")
 
-          elif choice == "3":
-               # Assuming you need to provide the token ID for logout
-               token_id = input("\nEnter your token ID: ")
-               logout_response = peer.logout(token_id)
-               print(logout_response,"\n")
+        elif choice == "2":
+            username = input("\nEnter username: ")
+            password = input("\nEnter password: ")
+            login_response = peer.login(username, password)
+            print("\nYour connection's ID is:",login_response,"\n")
 
-          elif choice == "4":
-               list_response = peer.list()
-               print(list_response,"\n")
+        elif choice == "3":
+            # Assuming you need to provide the token ID for logout
+            token_id = input("\nEnter your token ID: ")
+            logout_response = peer.logout(token_id)
+            print(logout_response,"\n")
 
-          elif choice == "5":
-               filename = input("\nEnter filename: ")
-               details_response = peer.details(filename)
-               print(details_response,"\n")
+        elif choice == "4":
+            list_response = peer.list()
+            print(list_response,"\n")
 
-          elif choice == "6":
-               print("\nExiting...")
-               break
+        elif choice == "5":
+            filename = input("\nEnter filename: ")
+            details_response = peer.details(filename)
+            print(details_response,"\n")
 
-          else:
-               print("\nInvalid choice. Please try again.")
+        elif choice == "6":
+            print("\nExiting...")
+            break
 
-"""
-TODO:
-     register a new user  ~~~> DONE 
-     login  ~~~> DONE 
-     list available files 
-     get details of a file 
-     logout 
-"""
+        else:
+            print("\nInvalid choice. Please try again.")
--- src/tracker.py
+++ src/tracker.py
@@ -4,123 +4,123 @@ import json
 import random
 
 class Tracker:
-     def __init__(self, port):
-          self.port = port    
-          self.peers = {}  # Stores info about the connected peers
-          self.files = {}  # Stores info about the available files
-          self.connected_ids = []  # List to store currently connected token IDs
+    def __init__(self, port):
+        self.port = port
+        self.peers = {}  # Stores info about the connected peers
+        self.files = {}  # Stores info about the available files
+        self.connected_ids = []  # List to store currently connected token IDs
 
-     def handle_peer(self, connection, address):
-          while True:
-               data = connection.recv(1024)
-               if not data:
-                    break
-               message = json.loads(data.decode())
-               self.process_message(message, connection, address)
-          connection.close()
+    def handle_peer(self, connection, address):
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            message = json.loads(data.decode())
+            self.process_message(message, connection, address)
+        connection.close()
 
-     # Type of messages
-     def process_message(self,message,connection,address):
-          action = message.get('action')
-          if action == 'register':
-               self.register(message, connection, address)
-          elif action == 'login':
-               self.login(message,connection,address)
-          elif action == 'logout':
-               self.logout(message,connection)
-          elif action == 'list':
-               self.list(connection)
-          elif action == 'details':
-               self.details(message, connection)
-          
-     # Registration function 
-     def register(self, message, connection, address):
-          user_name = message.get("user_name")
-          password = message.get("password")  # Extract password from message
-          if user_name in self.peers:
-               response = {'status': 'error', 'message': 'Username already exists'}
-          else:
-               self.peers[user_name] = {'password': password, 'address': address}  # Store password along with user info
-               response = {'status': 'success', 'message': 'Registration successful'}
-          connection.sendall(json.dumps(response).encode())
-     
-     
-     def login(self, message, connection, address):
-          user_name = message.get("user_name")
-          password = message.get("password")
-          if user_name in self.peers:
-               if self.peers[user_name]['password'] == password:
-                    token_id = random.randint(1, 1000)
-                    print("New Connection's Token ID:", token_id)
-                    self.peers[user_name]['token_id'] = token_id
-                    self.connected_ids.append(token_id)
-                    print("Connected IDs:", self.connected_ids)  # Checking if id is added correctly 
-                    response = {'status': 'success', 'token_id': token_id}
-               else:
-                    response = {'status': 'error', 'message': 'Incorrect password'}
-          else:
-               response = {'status': 'error', 'message': 'Username not found'}
-          connection.sendall(json.dumps(response).encode())
-          return token_id
+    # Type of messages
+    def process_message(self, message, connection, address):
+        action = message.get('action')
+        if action == 'register':
+            self.register(message, connection, address)
+        elif action == 'login':
+            self.login(message, connection, address)
+        elif action == 'logout':
+            self.logout(message, connection)
+        elif action == 'list':
+            self.list(connection)
+        elif action == 'details':
+            self.details(message, connection)
+        elif action == 'add_file':
+            self.add_file(message, connection)
 
+    # Registration function
+    def register(self, message, connection, address):
+        user_name = message.get("user_name")
+        password = message.get("password")  # Extract password from message
+        if user_name in self.peers:
+            response = {'status': 'error', 'message': 'Username already exists'}
+        else:
+            self.peers[user_name] = {'password': password, 'address': address}  # Store password along with user info
+            response = {'status': 'success', 'message': 'Registration successful'}
+        connection.sendall(json.dumps(response).encode())
 
-     
-     # logout function 
-     def logout(self, message,connection):
-          token_id = int(message.get('token_id'))  # Convert to integer
-          print("Token ID to logout:", token_id)
-          print("Connected IDs before logout:", self.connected_ids)
+    def login(self, message, connection, address):
+        user_name = message.get("user_name")
+        password = message.get("password")
+        if user_name in self.peers:
+            if self.peers[user_name]['password'] == password:
+                token_id = random.randint(1, 1000)
+                print("New Connection's Token ID:", token_id)
+                self.peers[user_name]['token_id'] = token_id
+                self.connected_ids.append(token_id)
+                print("Connected IDs:", self.connected_ids)  # Checking if id is added correctly
+                response = {'status': 'success', 'token_id': token_id}
+            else:
+                response = {'status': 'error', 'message': 'Incorrect password'}
+        else:
+            response = {'status': 'error', 'message': 'Username not found'}
+        connection.sendall(json.dumps(response).encode())
+        return token_id
 
-          if token_id in self.connected_ids:
-               self.connected_ids.remove(token_id)
-               print("Connected IDs after logout:", self.connected_ids)
+    # logout function
+    def logout(self, message, connection):
+        token_id = int(message.get('token_id'))  # Convert to integer
+        print("Token ID to logout:", token_id)
+        print("Connected IDs before logout:", self.connected_ids)
 
-               for user_name, peer_info in self.peers.items():
-                    if peer_info.get('token_id') == token_id:
-                         del self.peers[user_name]['token_id']
-                         print(f"Token ID {token_id} removed for user {user_name}")
+        if token_id in self.connected_ids:
+            self.connected_ids.remove(token_id)
+            print("Connected IDs after logout:", self.connected_ids)
 
-               response = {'status': 'success', 'message': 'Logout successful'}
-               connection.sendall(json.dumps(response).encode())  # Send the response back to the peer
-          else:
-               print("Token ID not found in connected IDs list")
-               response = {'status': 'error', 'message': 'Token ID not found in connected IDs list'}
-               connection.sendall(json.dumps(response).encode())  # Send the error response back to the peer
+            for user_name, peer_info in self.peers.items():
+                if peer_info.get('token_id') == token_id:
+                    del self.peers[user_name]['token_id']
+                    print(f"Token ID {token_id} removed for user {user_name}")
 
-          print("Logout response:", response)  # Add this line for debugging
-          return response
+            response = {'status': 'success', 'message': 'Logout successful'}
+            connection.sendall(json.dumps(response).encode())  # Send the response back to the peer
+        else:
+            print("Token ID not found in connected IDs list")
+            response = {'status': 'error', 'message': 'Token ID not found in connected IDs list'}
+            connection.sendall(json.dumps(response).encode())  # Send the error response back to the peer
 
+        print("Logout response:", response)  # Add this line for debugging
+        return response
 
+    def list(self, connection):
+        files = list(self.files.keys())
+        connection.sendall(json.dumps(files).encode())
 
+    def details(self, message, connection):
+        filename = message.get('filename')
+        if filename in self.files:
+            details = self.files[filename]
+            connection.sendall(json.dumps(details).encode())
+        else:
+            response = {'status': 'error', 'message': 'File not found'}
+            connection.sendall(json.dumps(response).encode())
 
+    def add_file(self, message, connection):
+        filename = message.get('filename')
+        file_details = message.get('file_details')
+        self.files[filename] = file_details
+        response = {'status': 'success', 'message': f'File {filename} added successfully'}
+        connection.sendall(json.dumps(response).encode())
 
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind(('localhost', self.port))
+            server_socket.listen()
+            print(f"Tracker is listening on port {self.port}...")
 
-     def list(self, connection):
-          files = list(self.files.keys())
-          connection.sendall(json.dumps(files).encode())
-
-     def details(self, message, connection):
-          filename = message.get('filename')
-          if filename in self.files:
-               details = self.files[filename]
-               connection.sendall(json.dumps(details).encode())
-          else:
-               response = {'status': 'error', 'message': 'File not found'}
-               connection.sendall(json.dumps(response).encode())
-
-     def start(self):
-          with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-               server_socket.bind(('localhost', self.port))
-               server_socket.listen()
-               print(f"Tracker is listening on port {self.port}...")
-
-               while True:
-                    connection, address = server_socket.accept()
-                    print(f"Connected to {address}")
-                    thread = threading.Thread(target = self.handle_peer, args = (connection, address))
-                    thread.start()
-               
+            while True:
+                connection, address = server_socket.accept()
+                print(f"Connected to {address}")
+                thread = threading.Thread(target=self.handle_peer, args=(connection, address))
+                thread.start()
 
 if __name__ == "__main__":
-    tracker = Tracker(5000)  # Port number 
+    tracker = Tracker(5000)  # Port number
     tracker.start()
